c712ee43c750257e2634b4048934af5ac8aab087
atokos <alext_1995@hotmail.com>
2019-02-07 15:48:33 +0200

    Fixed namespaceing of accounts and posts module

--- posts/urls.py
+++ posts/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from .views import PostUpdateView, PostCreateView, PostDetailView, PostListView
 
+
+app_name = 'posts'
+
 urlpatterns = [
-    path('edit/<int:pk>/', PostUpdateView.as_view(), name='posts-update'),
-    path('new/', PostCreateView.as_view(), name='posts-create'),
-    path('<int:pk>/', PostDetailView.as_view(), name='posts-detail'),
-    path('', PostListView.as_view(), name='posts-list'),
+    path('edit/<int:pk>/', PostUpdateView.as_view(), name='update'),
+    path('new/', PostCreateView.as_view(), name='create'),
+    path('<int:pk>/', PostDetailView.as_view(), name='detail'),
+    path('', PostListView.as_view(), name='list'),
 ]

a58e2f417117fbf8122752461b6da0ac5dbba97d
atokos <alext_1995@hotmail.com>
2019-02-07 11:02:17 +0200

    Moved template folder to module folder, to follow django best practices

--- src/blog_project/settings.py
+++ src/blog_project/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'posts/templates'),
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

7717e68d33aeecfd452d32aaf8f86b2063699c77
atokos <alext_1995@hotmail.com>
2019-02-07 11:01:30 +0200

    Added view for homepage & bootstrap

--- src/blog_project/urls.py
+++ src/blog_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import HomePageView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
+    path('', HomePageView.as_view(), name='homepage'),
 ]

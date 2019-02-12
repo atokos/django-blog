eb3c30cd12b6b55ce5bff342c72960e8dc55556b
atokos <alext_1995@hotmail.com>
2019-02-12 17:56:45 +0200

    Update allowed hosts

--- blog_project/settings/production.py
+++ blog_project/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u5xje$xw-$f1-qo-=ar_m&3@cc&)wv#-^+abz__+*(3oh2=0qz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['djogger.herokuapp.com']
 
 
 # Application definition
